d84cc56c3955bb5501e5b98b7f883fa9c3566a48
Nikolaj Majorov <nmajorov@users.noreply.github.com>
2022-02-04 17:46:34 +0100

    add route variable

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -3,23 +3,21 @@ using System.Text.Encodings.Web;
 
 namespace aspmvc.Controllers
 {
+    [Route("Hello")]
     public class HelloController:Controller 
     {
-        // 
-        // GET: /Hello/
-
-        [HttpGet("Hello/")]
+        [Route("")]
         public IActionResult Index(){
             return new ContentResult {Content ="Hello"};
         }
         
-        [HttpGet("Hello/Welcome")]
+        [Route("Welcome")]
         public IActionResult Welcome(){
             return new ContentResult {Content ="welcome"};
         }
 
 
-        [Route("Hello/blog/{year:int}/{month:int}/{key}")]
+        [Route("blog/{year:int}/{month:int}/{key}")]
         public IActionResult Post(int year,int month,string key){
             return new ContentResult { Content = string.Format("Year: {0}; Month {1}; Key: {2}",year,month,key)};  
         }
