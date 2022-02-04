6020e9f5a926c6fdcea52fe91357e7c0caa733c9
Nikolaj Majorov <nmajorov@users.noreply.github.com>
2022-02-04 17:42:32 +0100

    add route

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -17,5 +17,11 @@ namespace aspmvc.Controllers
         public IActionResult Welcome(){
             return new ContentResult {Content ="welcome"};
         }
+
+
+        [Route("Hello/blog/{year:int}/{month:int}/{key}")]
+        public IActionResult Post(int year,int month,string key){
+            return new ContentResult { Content = string.Format("Year: {0}; Month {1}; Key: {2}",year,month,key)};  
+        }
     }
 }
